1afffe99c96f7844a195e082d7971cd158e98cb0
Sakai Takao <s.takao.73+github@gmail.com>
2019-08-09 15:21:15 +0900

    hajimemashite.

---
+++ mkvenv.src.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+PYTHON_VENV_NAME=Jupyter
+PYTHON_VERSION=3.7.4
+
+pyenv virtualenvs | grep $PYTHON_VENV_NAME > /dev/null
+found=$?
+
+if [ $found -ne 0 ]; then
+    pyenv virtualenv $PYTHON_VERSION $PYTHON_VENV_NAME
+fi
+
+pyenv activate $PYTHON_VENV_NAME
+
+pip install -U jupyterlab
+pip install -U numpy==1.16.2
+pip install -U scipy
+pip install -U tensorflow-gpu
+pip install -U matplotlib
